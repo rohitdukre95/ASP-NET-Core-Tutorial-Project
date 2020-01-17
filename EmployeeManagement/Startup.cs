85cfd8b04764a866bf44685b922bb0357fbad909
rohitdukre95 <rohit.dukre@atidan.com>
2020-01-17 17:59:00 +0530

    Removed space

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -22,7 +22,6 @@ namespace EmployeeManagement
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-
         }
 
         public IConfiguration Configuration { get; }
